8cb5b598e044124940a6afe6c730a8182e73d525
Jefferson Benzan Tapia <jeffersonbt20@gmail.com>
2018-05-14 06:02:57 -0400

    Se agrego funcionalidad para insertar en la db, para actualizar, para consultar Se agrego iframe y reloj funcional a pagina queda pendiente consultar url desde la db

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,7 +6,7 @@ var span = document.getElementsByClassName("close")[0];
 
 var close = document.getElementById("botoncerrarmodal");
 
-// When the user clicks on <span> (x), close the modal
+// When the username clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
@@ -23,7 +23,7 @@ $('#boton_aceptar').click(function(event) {
         url: 'set_status', //server url
         type: 'POST', //passing data as post method
         dataType: 'json', // returning data as json
-        data: { user: $('#user').val(), commentary: $('#commentary').val(), 'urladdress': $('#urladdress').val(), initial_time: $('#initial_time').val(), evict_time_hasta: $('#evict_time_hasta').val(), status: "acceptet" }, //form values
+        data: { id: $('#id').val(), username: $('#username').val(), commentary: $('#commentary').val(), 'urladdress': $('#urladdress').val(), initial_time: $('#initial_time').val(), final_time: $('#final_time').val(), status: "acceptet" }, //form values
         success: function(json) {
             console.log("He aqui mi respuesta")
             console.log(json.result); //response from the server given as alert message
@@ -43,7 +43,7 @@ $('#boton_rechazar').click(function(event) {
         url: 'set_status', //server url
         type: 'POST', //passing data as post method
         dataType: 'json', // returning data as json
-        data: { user: $('#user').val(), commentary: $('#commentary').val(), 'urladdress': $('#urladdress').val(), initial_time: $('#initial_time').val(), evict_time_hasta: $('#evict_time_hasta').val(), status: "rejected" }, //form values
+        data: { id: $('#id').val(), username: $('#username').val(), commentary: $('#commentary').val(), 'urladdress': $('#urladdress').val(), initial_time: $('#initial_time').val(), final_time: $('#final_time').val(), status: "rejected" }, //form values
         success: function(json) {
             console.log("He aqui mi respuesta")
             console.log(json.result); //response from the server given as alert message
@@ -63,10 +63,16 @@ evtSrc.onmessage = function(e) {
     console.log(e.data);
     var datos = JSON.parse(e.data);
     console.log("Los datos son:", datos)
-
-    // Find a <table> element with id="myTable":
     var table = document.getElementById("body-solicitud");
-    var arrdatos = [datos.user, datos.urladdress, datos.commentary, datos.initial_time, datos.tiempohasta, datos.status]
+    if (datos.erase) {
+        console.log("Se mando a borrar", e.erase);
+        table.innerHTML = "";
+        return
+    } else {
+        console.log("No vino nada en borrr", e.erase)
+    }
+    // Find a <table> element with id="myTable":
+    var arrdatos = [datos.username, datos.urladdress, datos.commentary, datos.initial_time, datos.final_time, datos.status]
 
     var tr = document.createElement("tr");
 
@@ -96,25 +102,49 @@ evtSrc.onmessage = function(e) {
             var inputs = modal.querySelectorAll("input");
             var textarea = modal.querySelector("textarea");
             textarea.value = self.data.commentary;
-            inputs[0].value = self.data.user;
+            inputs[0].value = self.data.username;
             inputs[1].value = self.data.urladdress;
             inputs[2].value = self.data.initial_time;
-            inputs[3].value = self.data.tiempohasta;
+            inputs[3].value = self.data.final_time;
+            inputs[4].value = self.data.id;
 
-            console.log(inputs)
+            console.log("El status es:", self.data.status)
 
+            var restante = document.getElementById("restante");
+
+            var inicio = new Date(self.data.initial_time)
+            var fin = new Date(self.data.final_time)
+            var distance = new Date(fin.getTime() - inicio.getTime()); //+ 10800000);
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+            restante.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            console.log("El tiempo restante es:", restante.innerHTML);
+
+            var botones = document.getElementById("botones");
+
+            if (self.data.status != "registered") {
+                botones.style.display = "none"
+
+            } else {
+                botones.style.display = "block"
+
+            }
             modal.style.display = "block";
 
         }
-        // When the user clicks anywhere outside of the modal, close it
+        // When the username clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
         }
         //.srcElement.childNodes[0].nodeValue
-    console.log('------------------------------------');
-    console.log();
+    console.log("_______________________________________________________________");
     console.log('------------------------------------');
 
 };
